88895626f6458ba954b292158d40662c0443ce0c
linton-dawson <pushkarkukde@gmail.com>
2022-09-25 21:50:26 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,1 @@
+	g++ -Wall -std=c++17 -I"./libs/" src/main.cpp -lSDL2 -lSDL2_image -lSDL2_mixer -llua5.3 -o main
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include "imgui/imgui.h"
+#include <sol/sol.hpp>
+int main() {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	std::cout<<"Jello World !"<<std::endl;
+	return 0;
+}
+
