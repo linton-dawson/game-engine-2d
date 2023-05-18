3c4ad4c74bc5bc8cd4deaf140ea94f66955a0269
linton-dawson <pushkarkukde@gmail.com>
2023-05-19 01:00:27 +0200

    impl: introduce z-index for sprites

--- src/Components/SpriteComponent.hpp
+++ src/Components/SpriteComponent.hpp
@@ -5,14 +5,15 @@
 #include<string>
 struct SpriteComponent {
     std::string assetId;
-    int w,h;
+    int w,h, zIndex;
     SDL_Rect srcRect;
     
-    SpriteComponent(std::string assetId = "", int w = 0, int h = 0, int srcRectX = 0, int srcRectY = 0) {
+    SpriteComponent(std::string assetId = "", int w = 0, int h = 0, int srcRectX = 0, int srcRectY = 0, int zIndex=0) {
         this->assetId = assetId;
         this->w = w;
         this->h = h;
         this->srcRect = {srcRectX, srcRectY, w, h};
+        this->zIndex = zIndex;
     }
 };
 
