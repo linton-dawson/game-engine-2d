57c656a2ace76d8af017de5697c38f71fec3c68c
linton-dawson <pushkarkukde@gmail.com>
2023-07-04 23:30:14 +0200

    impl: add damage system

---
+++ src/Systems/DamageSystem.hpp
@@ -0,0 +1,28 @@
+#ifndef DAMAGESYSTEM_HPP
+#define DAMAGESYSTEM_HPP
+
+#include "../ECS/ECS.hpp"
+#include "../Components/BoxColliderComponent.hpp"
+#include "../EventBus/EventBus.hpp"
+#include "../Events/CollisionEvent.hpp"
+
+class DamageSystem: public System {
+    public:
+        DamageSystem() {
+            requireComponent<BoxColliderComponent>();
+        }
+
+        void subscribeToEvents(std::unique_ptr<EventBus>& eventBus) {
+            eventBus->subscribeToEvent<CollisionEvent>(this, &DamageSystem::onCollision);
+        }
+
+        void onCollision(CollisionEvent& event) {
+            Logger::Log("damage system received " + std::to_string(event.a.getId()) + " and " + std::to_string(event.b.getId()));
+            event.a.kill();
+            event.b.kill();
+        }
+
+        void update() {
+        }
+};
+#endif
